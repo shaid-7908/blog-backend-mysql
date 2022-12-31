219f5a0f8485ed6117adf10dae6cab9b99240622
Shahid <shahid451998@gmail.com>
2022-12-31 12:25:34 +0530

    Login route added

--- Api/Authentication.js
+++ Api/Authentication.js
@@ -1,6 +1,12 @@
 const CryptoJS=require("crypto-js")
 const jwt = require("jsonwebtoken");
+
+
 exports.Register=Register
+exports.Login=Login
+
+
+
 function Register(req, res){
  let { userName,userEmail ,userPassword,isAdmin,userImage,firstName,lastName} = req.body
 
@@ -60,4 +66,92 @@ function Register(req, res){
     })
    }
  })
+}
+
+function Login(req,res){
+ let {userName,userEmail,userPassword}=req.body
+if(userName){
+
+conn.query(`SELECT *,count(userId) as countt FROM users where userName=?`,[userName],(err,result)=>{
+    if(err) console.log(err)
+    if(result[0].countt != 0){
+        const haspass= CryptoJS.AES.decrypt(result[0].userpassword, process.env.PASS_SECRET)
+        const Orpassword=haspass.toString(CryptoJS.enc.Utf8)
+        if(userPassword != Orpassword) {
+            res.send({
+                status:400,
+                data:{},
+                message:'Wrong password'
+            })
+        }else{
+            let token = jwt.sign(
+                { userId: result[0].userId, userName: result[0].userName,isAdmin:result[0].isAdmin },
+                process.env.PASS_SECRET
+              );
+              res.send({
+                status:200,
+                data:{
+                    token,
+                    userId:result[0].userId,
+                    isAdmin:result[0].isAdmin,
+                    userName:result[0].userName,
+                    firstName:result[0].firstName,
+                    lastName:result[0].lastName
+                },
+                message:'Login success full'
+              })
+        }
+    }else{
+        res.send({
+            status:400,
+            data:{},
+            message:'Invalid user name'
+        })
+    }
+})
+
+}
+
+if(userEmail){
+    conn.query(`SELECT *,count(userId) as countt FROM users where userEmail=?`,[userEmail],(err,result)=>{
+        if(err) console.log(err)
+        if(result[0].countt != 0){
+            const haspass= CryptoJS.AES.decrypt(result[0].userpassword, process.env.PASS_SECRET)
+            const Orpassword=haspass.toString(CryptoJS.enc.Utf8)
+            if(userPassword != Orpassword) {
+                res.send({
+                    status:400,
+                    data:{},
+                    message:'Wrong password'
+                })
+            }else{
+                let token = jwt.sign(
+                    { userId: result[0].userId, userName: result[0].userName,isAdmin:result[0].isAdmin },
+                    process.env.PASS_SECRET
+                  );
+                  res.send({
+                    status:200,
+                    data:{
+                        token,
+                        userId:result[0].userId,
+                        isAdmin:result[0].isAdmin,
+                        userName:result[0].userName,
+                        firstName:result[0].firstName,
+                        lastName:result[0].lastName
+                    },
+                    message:'Login success full'
+                  })
+            }
+        }else{
+            res.send({
+                status:400,
+                data:{},
+                message:'Email not registered'
+            })
+        }
+    })
+
+
+}
+
 }
--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authentiaction=require('./Api/Authentication')
 
 router.post('/register',authentiaction.Register)
+router.post('/login',authentiaction.Login)
 
 module.exports=router
